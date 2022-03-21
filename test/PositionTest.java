e8cdbbfa6664307d56efb7345fd7622b4688ce03
victorLath <victor.lath9@etu.univ-lorraine.fr>
2022-03-21 13:29:11 +0100

    Ajout test position négative

--- test/PositionTest.java
+++ test/PositionTest.java
@@ -4,10 +4,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class PositionTest {
 
     @Test
-    public void testDeplacer(){
-        Position p = new Position(2,3);
+    public void testDeplacerPositionPositive(){
+        Position p = new Position(2,6);
         p.deplacer(Labyrinthe.HAUT);
-        assertEquals(p.getDx(),9,"x devrait etre egal 1");
+        assertEquals(1,p.getDx(),"x devrait etre egal 1");
+        p.deplacer(Labyrinthe.BAS);
+        p.deplacer(Labyrinthe.GAUCHE);
+        assertEquals(2,p.getDx()," x != 2" );
+        assertEquals(5,p.getDy()," x != 2" );
+    }
+
+    @Test
+    public void testDeplacerPositionNegative(){
+        Position p = new Position(-4,-7);
+        p.deplacer(Labyrinthe.HAUT);
+        assertEquals(-3,p.getDx(),"x != -3");
+        p.deplacer(Labyrinthe.BAS);
+        p.deplacer(Labyrinthe.GAUCHE);
+        assertEquals(-4,p.getDx()," x != -4" );
+        assertEquals(-8,p.getDy()," x != -8" );
     }
 
 }
