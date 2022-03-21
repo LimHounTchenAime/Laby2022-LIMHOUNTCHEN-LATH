0c25b6b0c343af4da6a6ee9c8eef827f4a081de9
Castorsorcier <aimelimhountchen@gmail.com>
2022-03-21 11:57:03 +0100

    Ajout javadoc methodes

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -11,26 +11,58 @@ class Labyrinthe {
 
     //methodes
 
+    /**
+     * retourne un caractere decrivant le contenu de
+     * la case (x,y)
+     * @param x numero de ligne (ordonnee)
+     * @param y numero de colonne (abscisse)
+     * @return caractere
+     */
     char getChar(int x, int y) {
         throw new Error("TODO");
     }
 
+    /**
+     * donne les coordonnees de la case voisine de (x,y) selon la direction action
+     * @param x numero de ligne (ordonnee)
+     * @param y numero de colonne (abscisse)
+     * @param action direction (haut, bas, gauche, droite)
+     * @return coordonnes de la case voisine
+     */
     static int[] getSuivant(int x, int y, String action) {
         throw new Error("TODO");
     }
 
+    /**
+     * deplace le personnage lorsqu’il fait l’action action
+     * @param action direction (haut, bas, gauche, droite)
+     * @throws ActionInconnueException
+     */
     void deplacerPerso(String action) throws ActionInconnueException {
         throw new Error("TODO");
     }
 
+    /**
+     * retourne une chaine decrivant le labyrinthe actuel
+     * @return affichage du labyrinthe
+     */
     public String toString() {
         throw new Error("TODO");
     }
 
+    /**
+     * retourne true si et seulement si le jeu est fini
+     * @return etat du jeu fini
+     */
     public boolean etreFini() {
         throw new Error("TODO");
     }
 
+    /**
+     * permet de charger un labyrinthe a partir d’un fichier
+     * @param nom nom du fichier
+     * @return labyrinthe charge
+     */
     public static Labyrinthe chargerLabyrinthe(String nom) {
         throw new Error("TODO");
     }
