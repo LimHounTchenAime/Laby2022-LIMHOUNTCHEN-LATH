126ae9353b51811c03b90d69da3ae41cc3892e00
victorLath <victor.lath9@etu.univ-lorraine.fr>
2022-03-21 11:26:43 +0100

    Test nomenclature

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -1,28 +1,24 @@
-/** 
+/**
  * Squelette de classe labyrinthe
  */
-class Labyrinthe{
+class Labyrinthe {
 
 
     char getChar(int x, int y) {
         throw new Error("TODO");
     }
 
-
     static int[] getSuivant(int x, int y, String action) {
         throw new Error("TODO");
     }
-
 
     void deplacerPerso(String action) throws ActionInconnueException {
         throw new Error("TODO");
     }
 
-
     public String toString() {
         throw new Error("TODO");
     }
-
 
     public boolean etreFini() {
         throw new Error("TODO");
