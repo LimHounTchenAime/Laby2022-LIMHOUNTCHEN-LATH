ba4d759e5a6a85918ba533350e5a08b33b6e6ccf
victorLath <victor.lath9@etu.univ-lorraine.fr>
2022-03-21 13:32:40 +0100

    ajout commentaire

--- test/PositionTest.java
+++ test/PositionTest.java
@@ -4,10 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class PositionTest {
 
     @Test
-    public void testDeplacerPositionPositive(){
+    public void testDeplacerPositionPositive(){*
+        //Test si x se deplace vers le haut
         Position p = new Position(2,6);
         p.deplacer(Labyrinthe.HAUT);
         assertEquals(1,p.getDx(),"x devrait etre egal 1");
+        //Test de déplacement de x,y
         p.deplacer(Labyrinthe.BAS);
         p.deplacer(Labyrinthe.GAUCHE);
         assertEquals(2,p.getDx()," x != 2" );
@@ -16,6 +18,7 @@ class PositionTest {
 
     @Test
     public void testDeplacerPositionNegative(){
+        //Test de x avec des coordonnees negatives
         Position p = new Position(-4,-7);
         p.deplacer(Labyrinthe.HAUT);
         assertEquals(-3,p.getDx(),"x != -3");
