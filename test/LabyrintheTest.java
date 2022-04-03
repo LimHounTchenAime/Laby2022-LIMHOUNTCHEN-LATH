dbcd9bdc24c3dc068afb32404fd16cb669b2422e
Aimé <aime.lim-houn-tchen5@etu.univ-lorraine.fr>
2022-04-03 15:56:58 +0200

    Oubli Commit méthodes de test

---
+++ test/LabyrintheTest.java
@@ -0,0 +1,82 @@
+import java.io.*;
+
+import org.junit.jupiter.api.*;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LabyrintheTest {
+
+    @Test
+    public void testChargerLabyrintheLaby0() {
+        //chargement des donnees
+        Labyrinthe labyrinthe = Labyrinthe.chargerLabyrinthe("laby/laby0.txt");
+
+        //verification
+        assertEquals(labyrinthe.toString(), "XXXXXXX\n" + "XS....X\n" + "X..P..X\n" + "X.....X\n" + "XXXXXXX\n");
+    }
+
+    @Test
+    public void testChargerLabyrintheLaby1() {
+        //chargement des donnees
+        Labyrinthe labyrinthe = Labyrinthe.chargerLabyrinthe("laby/laby1.txt");
+
+        //verification
+        assertEquals(labyrinthe.toString(), "XXXXXXXXXX\n" +
+                "X.....XX.X\n" +
+                "X.XS.....X\n" +
+                "X......X.X\n" +
+                "X........X\n" +
+                "X..XXP...X\n" +
+                "XXXXXXXXXX\n");
+    }
+
+    @Test
+    public void testChargerLabyrintheLaby2() {
+        //chargement des donnees
+        Labyrinthe labyrinthe = Labyrinthe.chargerLabyrinthe("laby/laby2.txt");
+
+        //verification
+        assertEquals(labyrinthe.toString(), "XXXXXXXXXXXXXXXXXXXX\n" +
+                "XX.......X........XX\n" +
+                "X........X.........X\n" +
+                "X........X...X.....X\n" +
+                "X..X.....X.......SXX\n" +
+                "X......XXX.........X\n" +
+                "X...X....X.........X\n" +
+                "X..X..........X....X\n" +
+                "X.....X..X.........X\n" +
+                "XX.......XX........X\n" +
+                "X........X.........X\n" +
+                "X........X.........X\n" +
+                "XX...............PXX\n" +
+                "XXXXXXXXXXXXXXXXXXXX\n");
+    }
+
+    @Test
+    public void testGetSuivant(){
+        //initialisation des variables
+        int[] test=new int[2];
+        test[0]=0;
+        test[1]=1;
+
+        //verification methode a tester
+        assertArrayEquals(Labyrinthe.getSuivant(1, 1, Labyrinthe.HAUT), test);
+
+        test[0]=1;
+
+        //verification methode a tester
+        assertArrayEquals(Labyrinthe.getSuivant(0, 1, Labyrinthe.BAS), test);
+
+        test[1]=2;
+
+        //verification methode a tester
+        assertArrayEquals(Labyrinthe.getSuivant(1, 1, Labyrinthe.DROITE), test);
+
+        test[1]=1;
+
+        //verification methode a tester
+        assertArrayEquals(Labyrinthe.getSuivant(1, 2, Labyrinthe.GAUCHE), test);
+    }
+}
